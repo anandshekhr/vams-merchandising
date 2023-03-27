0bd9551893eafc1d2b545c05892abc01a8f41ede
anandshekhr <66034323+anandshekhr@users.noreply.github.com>
2023-03-27 11:18:12 +0530

    Add model.py product
    UPdate categories and tags

--- products/models.py
+++ products/models.py
@@ -13,10 +13,10 @@ load_dotenv()
 
 user = get_user_model()
 
-CATEGORIES = (('Deal of the day', 'Deal of the day'), ('best-seller', 'Best Seller'), ('Trend Products', 'Trend Products'), ('Featured Products', 'Featured Products'), ('Biscuits & Snacks', 'Biscuits & Snacks'), ('Bread & Bakery', 'Bread & Bakery'), ('Breakfast & Dairy', 'Breakfast & Dairy'), ('Frozen Foods', 'Frozen Foods'), ('Fruits & Vegetables',
-                                                                                                                                                                                                                                                                                                                                         'Fruits & Vegetables'), ('Grocery & Staples', 'Grocery & Staples'), ('Household Needs', 'Household Needs'), ('Meats & Seafood', 'Meats & Seafood'), ('Eggs Substitutes', 'Eggs Substitutes'), ('Honey Vegetables', 'Honey Vegetables'), ('Marmalades Staples', 'Marmalades Staples'), ('Sour Cream and Dips', 'Sour Cream and Dips'), ('Yogurt Seafood', 'Yogurt Seafood'),)
-TAGS = (('Vegetarian', 'Vegetarian'), ('Non-Vegetarian',
-                                       'Non-Vegetarian'), ('Organic', 'Organic'),)
+CATEGORIES = (('new-arrival', 'New Arrival'), ('best-seller', 'Best Seller'), ('trending', 'Trend Products'),
+              ('Featured Products', 'Featured Products'), ('kids-collection', 'Kids Collection'),)
+TAGS = (('cotton', 'Cotton'), ('synthetic',
+                               'Synthetic'), ('woolen', 'Woolen'), ('polyster', 'Polyster'),)
 
 
 def user_directory_path(instance, filename):
