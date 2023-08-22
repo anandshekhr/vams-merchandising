dec386c862d977086821a1e7a0b310589b49800b
anandshekhr <66034323+anandshekhr@users.noreply.github.com>
2023-08-22 15:20:27 +0530

    UPDATE bug fixed caching issue

--- Home/customBaseVariable.py
+++ Home/customBaseVariable.py
@@ -7,32 +7,24 @@ from django.shortcuts import get_object_or_404
 from django.views.decorators.cache import never_cache
 
 
+
 def addVariableBaseTemplate(request):
     try:
         cartitems = Order.objects.get(user=request.user.id, ordered=False)
-        wishlist_items = Wishlist.objects.get(user=request.user.id)
-        return {
-            'cart': cartitems,
-            'wishlist':wishlist_items,
-            }
+    
     except Order.DoesNotExist:
-        return {
-            'cart': {
-                'name':'Does not exist'
-            },
-            'wishlist':{
-                'name':'Does not exist'
-            }
-        }
-
+        cartitems = "Does not exist"
+    
+    
+    try:
+        wishlist_items = Wishlist.objects.get(user=request.user.id)
+    
     except Wishlist.DoesNotExist:
-        return {
-            'cart': {
-                'name': 'Does not exist'
-            },
-            'wishlist': {
-                'name': 'Does not exist'
-            }
-        }
+        wishlist_items = "Does not exist"
+
+    return {
+        'cart': cartitems,
+        'wishlist': wishlist_items,
+    }
 
     
