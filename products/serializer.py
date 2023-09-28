a4969d3e932e6cfc80de2f0e821caa0adc8c3416
anandshekhr <66034323+anandshekhr@users.noreply.github.com>
2023-09-28 13:58:05 +0530

    UPDATE search items href

--- products/serializer.py
+++ products/serializer.py
@@ -24,12 +24,40 @@ class ProductReviewAndRatingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductReviewAndRatings
         fields = '__all__'
-    
+
+class CategoriesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Categories
+        fields = '__all__'
+
+class CategorySubCategoriesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CategorySubCategories
+        fields = '__all__'
+
+class ProductSizeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductSize
+        fields = '__all__'
+
+class ProductTagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductTag
+        fields= '__all__'
+                           
 class ProductsSerializer(serializers.ModelSerializer):
 
     desc = serializers.SerializerMethodField()
     discounted_price = serializers.SerializerMethodField()
     image = serializers.SerializerMethodField()
+    subcategory = CategorySubCategoriesSerializer(many=True)
+    category = CategoriesSerializer(many=True)
+    available_sizes = ProductSizeSerializer(many=True)
+    tags = ProductTagSerializer(many=True)
+
 
     def get_desc(self,instance):
         return str(instance.desc.html)
